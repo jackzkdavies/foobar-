8afde0a38d0d3276654ce71a2ee6ba3664a874e5
jack Davies <jack.davies@moh.io>
2017-02-06 00:21:52 +1300

    Work on basic systems underlining high card game

--- Deck of Cards API/app/app.routes.js
+++ Deck of Cards API/app/app.routes.js
@@ -7,7 +7,7 @@ myApp.config(function($stateProvider, $urlRouterProvider) {
         views: {
           '': { templateUrl: './app/shared/main/main.html'},
           'header@home': { templateUrl: './app/shared/header/header.html'},
-          'highCard@home': { templateUrl: './app/components/highCard/highCard.html', controller:'highCardController' },
+          'highCard@home': { templateUrl: './app/components/highCard/highCard.html', controller:'HighCardController' },
           'footer@home': { templateUrl: './app/shared/footer/footer.html' }
        }
     });
